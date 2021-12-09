4b08ff159216b70428670e72a4db5486493da582
Dave Feucht <poetas@gmail.com>
2021-12-09 19:50:21 +0100

    Adds second part of exercise1

--- day1/depth_count.js
+++ day1/depth_count.js
@@ -1,12 +1,14 @@
 const depthCountArray = require('./depth_list.json').data;
 let numberOfIncreases = 0;
 
+const getWindowCount = index => {
+    return (depthCountArray[index] + depthCountArray[index + 1] + depthCountArray[index + 2]);
+}
+
 depthCountArray.forEach((count, index) => {
-    if (index !== 0) {
-        if (count > depthCountArray[index - 1]) {
+    if (getWindowCount(index) < getWindowCount(index + 1)) {
 	    ++numberOfIncreases;
 	}
-    }
 });
 
 console.log(numberOfIncreases);
