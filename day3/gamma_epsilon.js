025bdf6cacb97278b1bfae2ea6c1b5c6d727546c
Dave Feucht <dfeucht@lequest.com>
2021-12-10 16:37:17 +0100

    Adds second part of exercise3

--- day3/gamma_epsilon.js
+++ day3/gamma_epsilon.js
@@ -3,6 +3,8 @@ const binaryCodes = require('./binary_codes.json').codes;
 const codeLength = binaryCodes[0].length;
 const codeGammaArray = [];
 const codeEpsilonArray = [];
+let oxygenGeneratorArray = [...binaryCodes];
+let co2ScrubberArray = [...binaryCodes];
 
 for (let i = 0; i < codeLength; ++i) {
     let numZeroes = 0;
@@ -22,6 +24,42 @@ for (let i = 0; i < codeLength; ++i) {
         codeGammaArray.push(1);
         codeEpsilonArray.push(0);
     }
+
+    numZeroes = 0;
+    numOnes = 0;
+    oxygenGeneratorArray.forEach(code => {
+        if (parseInt(code.charAt(i)) === 0) {
+            ++numZeroes;
+        } else {
+            ++numOnes;
+        }
+    });
+
+    if (numZeroes > numOnes) {
+        const tempOxygenArray = oxygenGeneratorArray.filter(tempCode => tempCode.charAt(i) === '0');
+        oxygenGeneratorArray = tempOxygenArray.length > 0 ? tempOxygenArray : oxygenGeneratorArray;
+    } else {
+        const tempOxygenArray = oxygenGeneratorArray.filter(tempCode => tempCode.charAt(i) === '1');
+        oxygenGeneratorArray = tempOxygenArray.length > 0 ? tempOxygenArray : oxygenGeneratorArray;
+    }
+
+    numZeroes = 0;
+    numOnes = 0;
+    co2ScrubberArray.forEach(code => {
+        if (parseInt(code.charAt(i)) === 0) {
+            ++numZeroes;
+        } else {
+            ++numOnes;
+        }
+    });
+
+    if (numZeroes > numOnes) {
+        const tempCO2Array = co2ScrubberArray.filter(tempCode => tempCode.charAt(i) === '1');
+        co2ScrubberArray = tempCO2Array.length > 0 ? tempCO2Array : co2ScrubberArray;
+    } else {
+        const tempCO2Array = co2ScrubberArray.filter(tempCode => tempCode.charAt(i) === '0');
+        co2ScrubberArray = tempCO2Array.length > 0 ? tempCO2Array : co2ScrubberArray;
+    }
 }
 
 const codeGamma = codeGammaArray.join('');
@@ -30,3 +68,7 @@ const codeEpsilon = codeEpsilonArray.join('');
 console.log('Gamma: ', codeGamma);
 console.log('Epsilon: ', codeEpsilon);
 console.log('Multiplied: ', parseInt(codeGamma, 2) * parseInt(codeEpsilon, 2));
+
+console.log('Oxygen: ', oxygenGeneratorArray);
+console.log('CO2: ', co2ScrubberArray);
+console.log('Multiplied: ', parseInt(oxygenGeneratorArray[0], 2) * parseInt(co2ScrubberArray[0], 2));
